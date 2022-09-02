283a1f76c83218b1a2eac50fcfae2bf80341635f
ychai23 <yaunmingchai@gmail.com>
2022-09-02 16:40:46 -0400

    customize to scan all file in repo

--- maintainer-tool.py
+++ maintainer-tool.py
@@ -6,13 +6,15 @@ import argparse
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--path", help="directory path to inspect from, default: src/pybind/mgr/", action="store_true", default="src/pybind/mgr/")
+parser.add_argument("-p", "--path", help="directory path to inspect from, default: src/pybind/mgr/", default="src/pybind/mgr/")
+parser.add_argument("-f", "--file", help="file name to inspect from, default: module", default="module")
 parser.add_argument("-t", "--time", help="start time to inspect the data from, default: 2022-01-01", action="store_true", default="2022-01-01")
 parser.add_argument("-rh", "--RH", help="weather to filter RH employees, default: True", action="store_true", default=True)
 parser.add_argument("-o", "--output", help="output file to store results in, default: False", action="store_true", default=False)
 args = parser.parse_args()
 
 dir_path = args.path
+file = args.file
 time = args.time
 employee_only = args.RH
 if args.output:
@@ -20,26 +22,45 @@ if args.output:
     sys.stdout = open("output.txt", "w")
 
 # Open a file
-dirs = os.listdir( dir_path )
 output=""
 print(f"You are inspecting repo under path {dir_path}")
 
-#List the directories and their contributers
+
+"""
+This following block will inspect all files in a directory and run git blame
+The problem is that it takes a long time to run
+"""
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(dir_path):
+    for file in f:
+        path = os.path.join(r, file)
+
+        # exclude test files
+        if 'tests' not in path:
+            authors = sp.getoutput(f'git blame -e {path} | cut -d\'(\' -f 2 | cut -d \'2\' -f 1 | grep \'redhat.com\' | sed \'s/[<>]//g\' | sort | uniq;')
+            output += '\n==========\n' + path +'\n' + authors
+
+
+"""
+This is what we have originally
+"""
+# List the directories and their contributers
 for d in sorted(os.listdir(dir_path)):
     if os.path.isdir(os.path.join(dir_path, d)) and d != 'tests':
         d += '/'
-        output+='\n'+d +'\n'
-        output += sp.getoutput(f'git blame -e {dir_path}{d}/module.py | cut -d\'(\' -f 2 | cut -d \'2\' -f 1 | grep \'redhat.com\' | sed \'s/[<>]//g\' | sort | uniq;')
-       
- 
+        output += '\n=========='
+        output+='\n'+ d +'\n\n'
+        output += sp.getoutput(f'git blame -e {dir_path}{d}module.py | cut -d\'(\' -f 2 | cut -d \'2\' -f 1 | grep \'redhat.com\' | sed \'s/[<>]//g\' | sort | uniq;')
+
+
 for email in output.split('\n'):
     if '/' in email:
         print('==========\n'+email)
-    if 'redhat.com' in email:
+    if '@' in email:
         try:
             # Open a connection to the server. LDAP url.
             connect = lp.initialize('ldaps://ldap.corp.redhat.com')
-            print('connected..')
         except lp.LDAPError as e:
             print(e)
 
